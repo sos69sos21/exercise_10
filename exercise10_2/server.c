41f58d07d4df415a1922698f96d1a394573a87f5
Matthew Shilinski <matthewshilinski@Matthews-MacBook-Pro.local>
2023-11-06 00:11:40 -0500

    updates

--- exercise10_2/server.c
+++ exercise10_2/server.c
@@ -1,4 +1,3 @@
-
 /*****************************************************
  *
  *  CSCV352 - server.c
@@ -7,18 +6,19 @@
  *
 ******************************************************/
 #include <stdio.h>
-#include <string.h>	
-#include <unistd.h>	
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>	
+#include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
 
 ///////////////////////////////////////////////////////
 //                CONSTANTS
 ///////////////////////////////////////////////////////
-#define LISTENER_PORT  8080
+#define LISTENER_PORT 8080
 
+void SetBlockingMode(int fd);
 
 /*****************************************************
  *
@@ -30,74 +30,95 @@
  *      argv - main parameter for argument values
  *
 ******************************************************/
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-	int listener;
+    int listener;
     int connectedSocket;
     int addrLen;
     int bytesRead;
-	struct sockaddr_in serverAddr , clientAddr;
-	char data[2000];
-    struct sockaddr_in* pClientAddr;
-    char clientIp[32]; 
-	
-	// create the listening socket
-	listener = socket(AF_INET , SOCK_STREAM , 0);
-	if (listener == -1)
-	{
-		printf("Error creating socket: %d\n", errno);
-	}
+    struct sockaddr_in serverAddr, clientAddr;
+    char data[2000];
+    struct sockaddr_in *pClientAddr;
+    char clientIp[32];
 
-	serverAddr.sin_family = AF_INET;              // IP V4 addresses
-	serverAddr.sin_addr.s_addr = INADDR_ANY;      // don't bind to a specific address
-	serverAddr.sin_port = htons( LISTENER_PORT ); // set the listening port
-	
-    // bind the address information to the listener socket
-	if( bind(listener,(struct sockaddr *)&serverAddr , sizeof(serverAddr)) < 0)
-	{
-		perror("Error binding socket address information.\n");
-		return 1;
-	}
-	
-	// start the listening process
-	listen(listener, 3);
-	
-	printf("Waiting for incoming connections...\n");
+    // create the listening socket
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    if (listener == -1)
+    {
+        printf("Error creating socket: %d\n", errno);
+        return 1;
+    }
 
-    // initialize the size variable to the size of the sockaddr_in struct.
-	addrLen = sizeof(struct sockaddr_in);	
+    serverAddr.sin_family = AF_INET;              // IP V4 addresses
+    serverAddr.sin_addr.s_addr = INADDR_ANY;      // don't bind to a specific address
+    serverAddr.sin_port = htons(LISTENER_PORT);   // set the listening port
 
-	//accept connection from an incoming client
-	connectedSocket = accept(listener, (struct sockaddr *)&clientAddr, (socklen_t*)&addrLen);
-	if (connectedSocket < 0)
-	{
-        printf("accept returned: %d, %s\n", connectedSocket, strerror(errno));
-		return 1;
-	}
+    // bind the address information to the listener socket with error checking
+    if (bind(listener, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
+        perror("Error binding socket address information");
+        fprintf(stderr, "bind error: %s\n", strerror(errno));
+        close(listener);  // Close the listener socket to clean up
+        return 1;
+    }
 
-    pClientAddr = (struct sockaddr_in*)&clientAddr;
-    inet_ntop(AF_INET, &(pClientAddr->sin_addr), clientIp, INET_ADDRSTRLEN);
-	printf("Connection accepted from: %s\n", clientIp);
-	
-	// receive data from the client
-	while ((bytesRead = recv(connectedSocket, data, sizeof(data), 0)) > 0 )
-	{
-		// echo the string back to client
-		write(connectedSocket, data, strlen(data));
-	}
-	
+    // start the listening process
+    listen(listener, 3);
 
-	if (bytesRead == 0)
-	{
-		printf("Recv returned 0, client disconnected.\n");
-		fflush(stdout);
-	}
-	else if (bytesRead == -1)
-	{
-		perror("Recv function return an error\n");
-	}
-	
-	return 0;
+    printf("Waiting for incoming connections...\n");
+
+    while (1)
+    {
+        // initialize the size variable to the size of the sockaddr_in struct.
+        addrLen = sizeof(struct sockaddr_in);
+
+        // accept connection from an incoming client
+        connectedSocket = accept(listener, (struct sockaddr *)&clientAddr, (socklen_t *)&addrLen);
+        if (connectedSocket < 0)
+        {
+            printf("accept returned: %d, %s\n", connectedSocket, strerror(errno));
+            return 1;
+        }
+
+        // Set the connected socket to non-blocking mode
+        SetBlockingMode(connectedSocket);
+
+        pClientAddr = (struct sockaddr_in *)&clientAddr;
+        inet_ntop(AF_INET, &(pClientAddr->sin_addr), clientIp, INET_ADDRSTRLEN);
+        printf("Connection accepted from: %s\n", clientIp);
+
+		while(1){
+			// receive data from the client
+			if ((bytesRead = recv(connectedSocket, data, sizeof(data), 0)) > 0)
+			//if ((bytesRead = recv(connectedSocket, data, sizeof(data), 0) > 0))
+			{
+				// echo the string back to the client
+				write(connectedSocket, data, strlen(data));
+			}
+
+			if (bytesRead == 0)
+			{
+				printf("Recv returned 0, client disconnected.\n");
+				fflush(stdout);
+				break;
+			}
+
+			else if(errno == EAGAIN || errno == EWOULDBLOCK)
+			{
+				continue;
+			}
+
+			else if (bytesRead == -1)
+			{
+				perror("Recv function returned an error\n");
+			}
+		}
+
+        // Close the connected socket
+        close(connectedSocket);
+    }
+
+    return 0;
 }
 
 void SetBlockingMode(int fd)
@@ -105,7 +126,6 @@ void SetBlockingMode(int fd)
     // where socketfd is the socket you want to make non-blocking
     if (fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) | O_NONBLOCK) == -1)
     {
-		perror("Error setting socket to non-blocking mode.\n");
+        perror("Error setting socket to non-blocking mode.\n");
     }
-
 }
